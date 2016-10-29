e8f7135d533303c080890cf1b3ae39eef4fc9c49
kmsheng <kmsh3ng@gmail.com>
2016-10-29 12:36:47 +0800

    init

---
+++ src/build-image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COMMIT=${TRAVIS_COMMIT::8}
+REPO=karmapa/$(cat package.json | grep name | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | xargs)
+
+docker --version
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker build --no-cache -f Dockerfile -t $REPO:$TRAVIS_BUILD_NUMBER-$COMMIT .
+docker push $REPO
