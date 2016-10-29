bc6b3adadf7928ebf83b4029f218cdb1543d293c
kmsheng <kmsh3ng@gmail.com>
2016-10-29 12:49:39 +0800

    remove no-cache flag

--- src/build-image.sh
+++ src/build-image.sh
@@ -5,5 +5,5 @@ REPO=karmapa/$(cat package.json | grep name | head -1 | awk -F: '{ print $2 }' |
 
 docker --version
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build --no-cache -f Dockerfile -t $REPO:$TRAVIS_BUILD_NUMBER-$COMMIT .
+docker build -f Dockerfile -t $REPO:$TRAVIS_BUILD_NUMBER-$COMMIT .
 docker push $REPO
