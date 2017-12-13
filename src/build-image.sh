25b327eae460a2dd449b0e5a4d206296d67f5889
kmsheng <kmsh3ng@gmail.com>
2017-12-13 22:50:40 +0800

    update docker login args for newer docker version

--- src/build-image.sh
+++ src/build-image.sh
@@ -4,6 +4,6 @@ COMMIT=${TRAVIS_COMMIT::8}
 REPO=karmapa/$(cat package.json | grep name | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | xargs)
 
 docker --version
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
 docker build -f Dockerfile -t $REPO:$TRAVIS_BUILD_NUMBER-$COMMIT .
 docker push $REPO
