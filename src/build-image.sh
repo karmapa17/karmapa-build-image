b7656961be830587cb16b966075ceae971f60620
karmapa <karmapa@karmapade-MacBook-Pro.local>
2023-01-20 00:06:39 +0800

    add tage name when push

--- src/build-image.sh
+++ src/build-image.sh
@@ -6,4 +6,4 @@ REPO=karmapa/$(cat package.json | grep name | head -1 | awk -F: '{ print $2 }' |
 docker --version
 docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
 docker build -f Dockerfile -t $REPO:$TRAVIS_BUILD_NUMBER-$COMMIT .
-docker push $REPO
+docker push $REPO:$TRAVIS_BUILD_NUMBER-$COMMIT
